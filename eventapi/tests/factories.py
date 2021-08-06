c4600d6c8a820681a0e84f2b6328925086a1ff3a
Kyle Weeks <kylepw@gmail.com>
2021-08-05 20:21:42 -0400

    Add EventFactory for tests

---
+++ eventapi/tests/factories.py
@@ -0,0 +1,24 @@
+from django.utils import timezone
+import factory
+from factory import fuzzy
+
+from ..models import Event
+
+
+CATEGORIES = ('page interaction', 'form interaction')
+NAMES = ('submit', 'pageview', 'click')
+
+
+class EventFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Event
+    
+    session_id = factory.Faker('md5', raw_output=False)
+    category = factory.fuzzy.FuzzyChoice(CATEGORIES)
+    name = factory.fuzzy.FuzzyChoice(NAMES)
+    data = factory.Sequence(lambda n: {'host': f'www.consumerreports{n}.com', 'path': '/'})
+    timestamp = factory.Faker(
+        'date_time_this_year',
+        before_now=True,
+        tzinfo=timezone.get_current_timezone()
+    )
